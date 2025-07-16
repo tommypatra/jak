61ab5c2bc246187408fffb9d6f6a8e3eecaba892
tommypatra <tommyirawan.patra@gmail.com>
2025-07-16 14:03:02 +0800

    fix token dashboard fin

--- resources/views/template_dashboard.blade.php
+++ resources/views/template_dashboard.blade.php
@@ -100,8 +100,8 @@
 
   <!-- Footer -->
   @include('partials_footer')
-  <script src="{{ asset('js/token.js?v=4') }}"></script>
-  <script src="{{ asset('js/myapp.js?v=4') }}"></script>
+  <script src="{{ asset('js/token.js?v=5') }}"></script>
+  <script src="{{ asset('js/myapp.js?v=5') }}"></script>
   <script>
     var akses_grup = localStorage.getItem('akses_grup');
     var daftar_akses = JSON.parse(localStorage.getItem('daftar_akses'));
